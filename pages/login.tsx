dcd9ff2b3ca1aacfc1096ccb380f7917e3894b1d
Gervalla Adhurim (s) <adhurim.gervalla@students.fhnw.ch>
2021-12-14 17:00:51 +0100

    tab in code

--- pages/login.tsx
+++ pages/login.tsx
@@ -26,7 +26,6 @@ export default function Login(): ReactElement {
         )
     }
 
-
     // Logout Button Component which renders a button to logout
     function LogoutButton(): ReactElement {
         const logout = () => {
@@ -37,56 +36,56 @@ export default function Login(): ReactElement {
     }
 
     // Component that shows the Form to authenticate users
-function SignInWithEmailForm(): ReactElement {
-    const [loginWithEmailLink, setLoginWithEmailLink] = useState(false);
+    function SignInWithEmailForm(): ReactElement {
+        const [loginWithEmailLink, setLoginWithEmailLink] = useState(false);
 
-    // function which is triggered when the login form is submitted
-    const handleSubmit = (event) => new Promise<void>((resolve, reject) => {
-        event.preventDefault();
+        // function which is triggered when the login form is submitted
+        const handleSubmit = (event) => new Promise<void>((resolve, reject) => {
+            event.preventDefault();
 
-        const email = event.target.email.value;
-        const pw = event.target.password.value;
-        const sendEmailLoginLink = loginWithEmailLink;
+            const email = event.target.email.value;
+            const pw = event.target.password.value;
+            const sendEmailLoginLink = loginWithEmailLink;
 
-        const obj = {
-            loading: {
-                loading: 'Loading', success: () => `Successfully logged in`, 
-                error: (err) => `${err.toString()}`
-            }, 
-            styling: {
-                style: {minWidth: '250px'}, 
-                success: {duration: 5000, icon: '🔥'}
-            }
-        };
-        
-        // register via E-Mail and password field
-        if (!sendEmailLoginLink && email && pw) {
-            toast.promise(
-                signInWithEmailAndPassword(auth, email, pw),
-                obj.loading,
-                obj.styling
-            );       
-        } 
+            const obj = {
+                loading: {
+                    loading: 'Loading', success: () => `Successfully logged in`, 
+                    error: (err) => `${err.toString()}`
+                }, 
+                styling: {
+                    style: {minWidth: '250px'}, 
+                    success: {duration: 5000, icon: '🔥'}
+                }
+            };
+            
+            // register via E-Mail and password field
+            if (!sendEmailLoginLink && email && pw) {
+                toast.promise(
+                    signInWithEmailAndPassword(auth, email, pw),
+                    obj.loading,
+                    obj.styling
+                );       
+            } 
 
-        // send E-Mail Login Link to login
-        if (sendEmailLoginLink && email) {
-            toast.promise(
-                sendSignInLinkToEmail(auth, email, actionCodeSettings),
-                obj.loading,
-                obj.styling
-            )
-        } 
-    });
+            // send E-Mail Login Link to login
+            if (sendEmailLoginLink && email) {
+                toast.promise(
+                    sendSignInLinkToEmail(auth, email, actionCodeSettings),
+                    obj.loading,
+                    obj.styling
+                )
+            } 
+        });
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <input type="email" name="email" placeholder="E-Mail" required />
-            <input id="password" type={!loginWithEmailLink ? 'password' : 'hidden'} name="password" placeholder="Password" required={!loginWithEmailLink} />
-            <button type="submit">Login</button>
-            <label><input type="checkbox" name="emailLink" onChange={() => setLoginWithEmailLink(!loginWithEmailLink)} />Login with E-Mail Link</label>
-        </form>
-    );
-}
+        return(
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email" placeholder="E-Mail" required />
+                <input id="password" type={!loginWithEmailLink ? 'password' : 'hidden'} name="password" placeholder="Password" required={!loginWithEmailLink} />
+                <button type="submit">Login</button>
+                <label><input type="checkbox" name="emailLink" onChange={() => setLoginWithEmailLink(!loginWithEmailLink)} />Login with E-Mail Link</label>
+            </form>
+        );
+    }
 
     return(
         <main>
